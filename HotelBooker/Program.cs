e0d570c669dae3d6b3d52fb48c49428cb3780d69
GuilhermeJacobRech <guilhermejacobrech@gmail.com>
2025-01-13 18:13:32 +0100

    Added comments to compliment the code in Program.cs and removed comments that were redundant.

--- HotelBooker/Program.cs
+++ HotelBooker/Program.cs
@@ -7,35 +7,30 @@ namespace HotelBooker
     {
         static void Main(string[] args)
         {
-            // Reads .json files as string
-            string hotelsJson = Helpers.ReadJsonFile("hotels");
-            string bookingsJson = Helpers.ReadJsonFile("bookings");
+            string hotelsJson = JsonFileReader.ReadJsonFile("hotels");
+            string bookingsJson = JsonFileReader.ReadJsonFile("bookings");
 
-            // Instantiate repositories. They are responsible for deserializing the json string
             IHotelRepository hotelRepository = new JsonHotelRepository(hotelsJson);
             IBookingRepository bookingRepository = new JsonBookingRepository(bookingsJson);
 
             List<Hotel> hotels = hotelRepository.GetAll();
             if (hotels.Count > 0)
             {
-                // Print list of hotels 
-                Helpers.PrintHotelsWithDetails(hotels);
+                HotelDataPrinter.PrintHotelsWithDetails(hotels);
 
-                // Instantiate booking service that handles the logic for bookings
-                BookingService bookingService = new BookingService(hotelRepository, bookingRepository);
+                BookingService bookingService = new(hotelRepository, bookingRepository);
+                UserInputHandler userInputHandler = new(hotelRepository);
 
+                // Only way to exit the application is by the user entering a null or blank input.
                 while (true)
                 {
                     Console.WriteLine("Usage example: Availability(H1, 20240901-20240903, DBL)");
 
-                    // Get and deserialize user input
-                    string userInputString = Helpers.GetUserInput();
-                    UserInput? parsedInput = Helpers.DeserializeInput(userInputString);
+                    string userInputString = UserInputHandler.GetUserInput();
+                    UserInput? parsedInput = UserInputHandler.DeserializeInput(userInputString);
 
-                    // Validates user input
-                    if (bookingService.IsUserInputValid(parsedInput))
+                    if (userInputHandler.IsUserInputValid(parsedInput))
                     {
-                        // Print amount of available rooms
                         int availableRooms = bookingService.GetAvailableRoomsCount(parsedInput.HotelId, parsedInput.Arrival, parsedInput.Departure, parsedInput.RoomTypeCode);
                         Console.WriteLine($"Total available rooms: {availableRooms}");
                     }
